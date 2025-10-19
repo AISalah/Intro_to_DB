7b18556f978e597f621e6101a06ceb61f340cd7a
AISalah <salah.ais48@gmail.com>
2025-10-19 16:36:44 +0100

    A Database for Your Dream Online Bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,11 +29,12 @@ CREATE TABLE Orders (
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
+
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES  Books(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
