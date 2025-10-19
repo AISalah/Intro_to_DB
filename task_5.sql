ee51553e1b0c61ed892e03a2d42acd92215c9893
AISalah <salah.ais48@gmail.com>
2025-10-19 22:17:19 +0100

    Populating Database with the Very First Data

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,1 @@
-INSERT INTO Cusotmer (customer_id, customer_name, email, address) VALUES (1, 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
+INSERT INTO cusotmer (customer_id, customer_name, email, address) VALUES (1, 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
